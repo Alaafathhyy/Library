c64da83886d93ea93004032e912e44b9377693c9
Alaa fathy <57330162+Alaafathhyy@users.noreply.github.com>
2021-03-02 19:48:44 +0200

    Update Prim.cpp

--- Prim.cpp
+++ Prim.cpp
@@ -1,35 +1,24 @@
+bool vis[N];
+int n, m;
 
-vector<pair<int, ll>> adj[N];
- 
-ll dis[N];
-int n, m, k;
- 
-int vis[N], vid;
- 
-ll prim(int src = 0) {
-    ll ret = 0;
- 
-    vid++;
-    memset(dis, '?', n * sizeof dis[0]);
-    priority_queue<pair<ll, int>> q;
-    dis[src] = 0;
-    q.emplace(0, src);
+vector<pair<ll, ll>> vec[N];
+
+ll prim(int src) {
+    ll ans = 0;
+    priority_queue<pll, vector<pll >, greater<pll > > q;
+    vis[src] = 1;
+    for (auto v:vec[src])
+        q.emplace(v.second, v.first);
     while (!q.empty()) {
         int u = q.top().second;
-        ll d = -q.top().first;
+        ll c = q.top().first;
         q.pop();
-        if (d != dis[u]) continue;
-        ret += d;
-        vis[u] = vid;
-        for (auto &e : adj[u]) {
-            int v = e.first;
-            ll nd = e.second;
-            if (nd < dis[v] && vis[v] != vid) {
-                dis[v] = nd;
-                q.emplace(-nd, v);
-            }
+        if (vis[u])continue;
+        vis[u] = 1;
+        ans += c;
+        for (auto a:vec[u]) {
+            q.emplace(a.second, a.first);
         }
     }
-    return ret;
+    return ans;
 }
- 
